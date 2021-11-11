f9364a54b8ca655cb130e16c9ff8b6612a755ed4
TannerOliver <435.tanner@gmail.com>
2021-11-11 09:35:25 -0600

    adding exercise model

---
+++ model/Exercise.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//  Date, Name, Type, Weight, Sets, Reps, Duration, Distance Traveled for cardio.
+
+const ExerciseSchema = new Schema ({
+  type: {
+    type: String,
+    unique: false
+  },
+  name: {
+    type: String
+  },
+  duration: {
+    type: Number
+  },
+  weight: {
+    type: Number
+  },
+  reps: {
+    type: Number
+  },
+  sets: {
+    type: Number
+  },
+  date: {
+    type: Date,
+    defualt: Date.now
+  }
+});
+
+const Exercise = mongoose.model("Exercise", ExerciseSchema);
+
+module.exports = Exercise;
